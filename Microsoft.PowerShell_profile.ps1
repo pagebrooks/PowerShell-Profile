ac2631900cc54f8979d7eb50400c4a492d06bb35
Page Brooks <pagebrooks@gmail.com>
2012-03-13 10:40:03 -0700

    Initial Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,43 @@
+$gitInstallDir = 'C:\Program Files (x86)\Git\'
+$Env:HOME = $Env:USERPROFILE
+$Env:Path = "$gitInstallDir\cmd;$gitInstallDir\mingw\bin;$Env:Path"
+
+function touch($file) { "" | Out-File $file -Encoding ASCII }
+
+Import-Module $Env:HOME\Documents\WindowsPowerShell\Modules\posh-git
+
+function shorten-path([string] $path) { 
+   $loc = $path.Replace($HOME, '~') 
+   # remove prefix for UNC paths 
+   $loc = $loc -replace '^[^:]+::', '' 
+   # make path shorter like tabs in Vim, 
+   # handle paths starting with \\ and . correctly 
+   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2') 
+}
+
+function prompt { 
+   
+   $cdelim = [ConsoleColor]::DarkCyan 
+   $chost = [ConsoleColor]::Green 
+   $cloc = [ConsoleColor]::Cyan 
+
+   $hostName = [net.dns]::GetHostName()
+   $machineName = $hostName
+   if($hostName -eq 'CHS23L-F1723Q1') {
+		$machineName = 'BROPN-PSA'
+   }
+   
+   write-host ($machineName) -n -f $chost 
+   write-host ' ' -n -f $cdelim 
+   write-host (shorten-path (pwd).Path) -n -f $cloc 
+   
+   $global:GitStatus = Get-GitStatus
+   Write-GitStatus $GitStatus
+   
+   write-host ' $' -n -f $cdelim 
+   return ' ' 
+}
+
+Enable-GitColors
+
+
